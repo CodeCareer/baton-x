f56d1722eb7ae4f075b6c3d0f726a3cfe1de92f0
luxueyan <luxueyan@kaitongamc.com>
2017-05-19 10:39:58 +0800

    table padding modify

--- src/common/filter.js
+++ src/common/filter.js
@@ -1,12 +1,12 @@
-import _ from 'lodash'
+import { isNumber, round, isNil, isNull } from 'lodash'
 import moment from 'moment'
 import numeral from 'numeral'
 
 export default {
   install(Vue, options) {
     Vue.filter('ktCurrency', (value, prefix = '￥', suffix = '') => {
-      if (_.isNumber(value)) {
-        return prefix + numeral(_.round(value, 2)).format('0,0.00') + suffix
+      if (isNumber(value)) {
+        return prefix + numeral(round(value, 2)).format('0,0.00') + suffix
       } else {
         return value
       }
@@ -21,12 +21,12 @@ export default {
     })
 
     Vue.filter('ktPercent', (value, decimal = 2, multi = 100, unit = '%') => {
-      return _.isNumber(value) ? _.round(value * multi, decimal).toFixed(decimal) + (unit || '') : ''
+      return isNumber(value) ? round(value * multi, decimal).toFixed(decimal) + (unit || '') : ''
     })
 
     Vue.filter('ktRangePercent', (value, value2, decimal = 2) => {
-      value = _.isNumber(value) ? _.round(value, decimal).toFixed(decimal) : ''
-      value2 = _.isNumber(value2) ? _.round(value2, decimal).toFixed(decimal) : ''
+      value = isNumber(value) ? round(value, decimal).toFixed(decimal) : ''
+      value2 = isNumber(value2) ? round(value2, decimal).toFixed(decimal) : ''
       if (value === value2) {
         return value + '%'
       }
@@ -35,26 +35,26 @@ export default {
     })
 
     Vue.filter('ktRound', (value, decimal = 0) => {
-      return _.round(value, decimal).toFixed(2)
+      return round(value, decimal).toFixed(2)
     })
 
     Vue.filter('ktAppend', (value, str) => {
-      return (_.isNil(value) ? '' : value) + str
+      return (isNil(value) ? '' : value) + str
     })
 
     Vue.filter('ktPrepend', (value, str) => {
-      return str + (_.isNil(value) ? '' : value)
+      return str + (isNil(value) ? '' : value)
     })
 
     Vue.filter('ktPositveNumber', (value) => {
-      if (value > 0 && _.isNumber(value)) {
+      if (value > 0 && isNumber(value)) {
         return '+' + value
       }
       return value
     })
 
     Vue.filter('ktNull', (value, str) => {
-      return _.isNull(value) ? '-' : (str || value)
+      return isNull(value) ? '-' : (str || value)
     })
 
     Vue.filter('moment', (date, format) => {
